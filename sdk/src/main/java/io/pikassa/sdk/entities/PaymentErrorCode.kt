76190157170394699cba516a5bf05adfaecb1a92
denmusic1992 <denmusic92@gmail.com>
2020-06-26 19:00:43 +0700

    PIKA-7 add enum serialization

--- sdk/src/main/java/io/pikassa/sdk/entities/PaymentErrorCode.kt
+++ sdk/src/main/java/io/pikassa/sdk/entities/PaymentErrorCode.kt
@@ -1,14 +1,22 @@
 package io.pikassa.sdk.entities
 
+import com.google.gson.annotations.SerializedName
+
 /**
 Created by Denis Chornyy on 25,Июнь,2020
 All rights received.
  */
 enum class PaymentErrorCode(val code: String) {
+    @SerializedName("1")
     CreateAccountError("1"),
+    @SerializedName("2")
     AuthorizeAccountError("2"),
+    @SerializedName("3")
     CancelPaymentError("3"),
+    @SerializedName("4")
     ReceiveDataError("4"),
+    @SerializedName("5")
     PaybackError("5"),
+    @SerializedName("6")
     SystemError("6")
 }
--- sdk/src/main/java/io/pikassa/sdk/entities/PaymentMethod.kt
+++ sdk/src/main/java/io/pikassa/sdk/entities/PaymentMethod.kt
@@ -1,5 +1,7 @@
 package io.pikassa.sdk.entities
 
+import com.google.gson.annotations.SerializedName
+
 /**
 Created by Denis Chornyy on 25,Июнь,2020
 All rights received.
@@ -8,8 +10,12 @@ All rights received.
  * Class representing types of in-app payment
  */
 enum class PaymentMethod(val method: String) {
+    @SerializedName("BankCard")
     BANK_CARD("BankCard"),
+    @SerializedName("WMR")
     WEBMONEY("WMR"),
+    @SerializedName("YandexMoney")
     YANDEX_MONEY("YandexMoney"),
+    @SerializedName("Mobile")
     MOBILE("Mobile")
 }
