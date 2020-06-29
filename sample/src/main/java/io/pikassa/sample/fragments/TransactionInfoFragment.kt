c40f5b52bb5dd0de757dadc884bfbbc28ab9aba2
denmusic1992 <denmusic92@gmail.com>
2020-06-29 14:13:38 +0700

    PIKA-9 add Transaction Info Fragment with viewmodel

---
+++ sample/src/main/java/io/pikassa/sample/fragments/TransactionInfoFragment.kt
@@ -0,0 +1,31 @@
+package io.pikassa.sample.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import io.pikassa.sample.databinding.FragmentTransactionInfoBinding
+import io.pikassa.sample.viewmodels.TransactionInfoViewModel
+
+/**
+Created by Denis Chornyy on 29,Июнь,2020
+All rights received.
+ */
+class TransactionInfoFragment: BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val viewmodel: TransactionInfoViewModel by viewModels()
+
+        val binding = FragmentTransactionInfoBinding.inflate(inflater, container, false)
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = viewmodel
+        }
+        return binding.root
+    }
+}
---
+++ sample/src/main/java/io/pikassa/sample/viewmodels/TransactionInfoViewModel.kt
@@ -0,0 +1,10 @@
+package io.pikassa.sample.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+/**
+Created by Denis Chornyy on 29,Июнь,2020
+All rights received.
+ */
+class TransactionInfoViewModel: ViewModel() {
+}
